29a5c8d0cf93f519595c5b75fb6f3ad25fee2fb5
sengunkaancan <kaancan_90@hotmail.com>
2019-10-26 12:14:55 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World !");
+            //Console.ReadKey();
+            //Console.ReadLine();
+
+            Console.WriteLine(" Lütfen adınızı giriniz :");
+            Console.WriteLine("giriş yapan kullanıcı: " + Console.ReadLine());
+            Console.Read();
+
+        }
+    }
+}
